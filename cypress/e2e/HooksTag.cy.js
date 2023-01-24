5648362f86ae6e85a2c319fa5180cb8b24246d1f
SharmaShalini99 <sharma.shalini@magicfinserv.com>
2023-01-24 09:59:25 +0530

    testing..

--- cypress/e2e/HooksTag.cy.js
+++ cypress/e2e/HooksTag.cy.js
@@ -3,7 +3,7 @@ require ('@4tw/cypress-drag-drop')
 describe('Mouse Operations', () => 
 {
 
-    it.only('Mouse Hover' , ()=>{
+    it('Mouse Hover' , ()=>{
         cy.visit("https://demo.opencart.com/")
 
         cy.get('body>main:nth-child(3) >div:nth-child(1) >nav:nth-child(1) >div:nth-child(3) >ul:nth-child(1) >li:nth-child(1) > div:nth-child(2) >div:nth-child(1) >ul:nth-child(1) >li:nth-child(2) >a:nth-child(1)')
